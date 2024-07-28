502c94067e2acd1c03c4853a5a2c4077bf367da7
VladimirKroshka <namevova@yandex.ru>
2024-07-28 23:37:04 +0600

    Update RandomLoginGenerator.java

--- src/test/java/resources/RandomLoginGenerator.java
+++ src/test/java/resources/RandomLoginGenerator.java
@@ -7,8 +7,4 @@ public class RandomLoginGenerator {
     public static String generateRandomLogin() {
         return UUID.randomUUID().toString().replaceAll("-", "").substring(0, 6);
     }
-
-    public static void main(String[] args) {
-        String randomLogin = generateRandomLogin();
-    }
 }
